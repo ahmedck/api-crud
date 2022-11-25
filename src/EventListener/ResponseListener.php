83012d6f3fc7789a9f66fcc6f17cb4b813700ea7
ahmed <ahmedcherifkrid@gmail.com>
2022-11-25 08:13:58 +0100

    enable cors

--- src/EventListener/ResponseListener.php
+++ src/EventListener/ResponseListener.php
@@ -10,13 +10,24 @@ class ResponseListener
 {
     public function onKernelResponse(ResponseEvent $event)
     {
-        $response = $event->getResponse();
-           $response->headers->set('Access-Control-Allow-Origin', '*');
+		
+		$method = $event->getRequest()->getMethod();
+		if($method == "OPTIONS"){
+			$res = new Response();
+			$res->headers->set('Access-Control-Allow-Origin', '*'); 
+			$res->headers->set('Access-Control-Allow-Methods', 'POST,PUT,DELETE,GET,OPTIONS');
+            $event->setResponse($res);
+		}else{
+            $response = $event->getResponse();
+            $response->headers->set('Access-Control-Allow-Origin', '*');
             $response->headers->set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
             $response->headers->set('Access-Control-Allow-Methods', 'POST,PUT,DELETE,GET,OPTIONS');
-            $response->headers->set('Access-Control-Max-Age', 3600);
-			
-        // sends the modified response object to the event
-        $event->setResponse($response);
+	     	$response->headers->set('Access-Control-Max-Age', 3600);
+		
+		    // sends the modified response object to the event
+            $event->setResponse($response);
+		}
+	
+
     }
 }
